bea1e5e87f19074236078d1bb0982adc71176dbe
Sudheer455 <sudheerch455@gmail.com>
2024-08-08 13:00:14 +0530

    prime number progam file is added

---
+++ src/prime.java
@@ -0,0 +1,32 @@
+public class prime {
+    public static void main(String[] args) {
+        prime(null);
+    }
+    public static void prime(String[] args)
+    {
+        int i,m=0,flag=0;
+        int n=3;
+        m=n/2;
+        if(n==0||n==1)
+        {
+            System.out.println("not a prime number");
+        }
+        else{
+            for(i=2;i<=m;i++)
+            {
+                if(n%i==0)
+                {
+                    System.out.println("the number is not prime");
+                    flag=1;
+                    break;
+
+                }
+
+            }
+            if(flag==0)
+            {
+                System.out.println("the given number is prime");
+            }
+        }
+    }
+}
